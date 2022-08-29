9b38f21fbafd1b5e0545a4b5773c8e0d6360f2d5
wangweijie <wangweijie@163>
2022-08-29 17:31:22 +0800

    1.请求路径的方式进行修改

--- service/service_oss/src/main/java/com/huatian/ossService/controller/OssController.java
+++ service/service_oss/src/main/java/com/huatian/ossService/controller/OssController.java
@@ -5,10 +5,7 @@ import com.huatian.ossService.service.OssService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -32,14 +29,14 @@ public class OssController {
     @Autowired
     private OssService ossService;
 
-
     /**
      * 上传文件
+     *
      * @param file
      * @return 文件的链接
      */
     @ApiOperation("上传文件")
-    @PostMapping
+    @RequestMapping(value = "uploadFile", method = RequestMethod.POST)
     public Result uploadFile(MultipartFile file) {
         String url = ossService.uploadFile(file);
         Map<String, Object> map = new HashMap<>();
